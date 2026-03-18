de9002b85b6b4b7846407464c2e05afd1e240b88
Abhishek  Nigam <148670286+abhisheknigam5714@users.noreply.github.com>
2026-03-18 21:26:40 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
 
   public static void main(String[] args) {
     System.out.println("hello world of jenkins");
-    System.out.println("hi start the work baby");
+    System.out.println("hi k baby");
   }
 }
